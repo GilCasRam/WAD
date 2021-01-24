16386a9dadedd3423f7eab55a3d3a718a4915a0d
JesusBalVar <JesusBalVar@escom.github.com>
2021-01-24 02:22:06 -0600

    Agregando Reporte

--- src/main/java/com/ipn/mx/controlador/Mono_Bio_Servlet.java
+++ src/main/java/com/ipn/mx/controlador/Mono_Bio_Servlet.java
@@ -5,18 +5,24 @@
  */
 package com.ipn.mx.controlador;
 
+import com.ipn.mx.modelo.dao.Conexion;
 import com.ipn.mx.modelo.dao.Mono_BioDAO;
 import com.ipn.mx.modelo.dto.Mono_BioDTO;
+import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperRunManager;
 
 /**
  *
@@ -35,7 +41,7 @@ public class Mono_Bio_Servlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         String accion = request.getParameter("accion");
 
@@ -63,7 +69,11 @@ public class Mono_Bio_Servlet extends HttpServlet {
 
             mostrarMono(request, response);
 
+        } else if(accion.equals("verPDF")){
+            
+            verPDF(request, response);
         }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -196,6 +206,32 @@ public class Mono_Bio_Servlet extends HttpServlet {
         } catch (SQLException | ServletException | IOException ex) {
             Logger.getLogger(Mono_Bio_Servlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private void verPDF(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+        
+        Conexion conexion = new Conexion();
+        conexion.obtenerConexion_PostgreSQL();        
+        Connection con = conexion.getCon();
+        
+        try {
+            ServletOutputStream sos = response.getOutputStream();//SALIDA PARA EL REPORTE PDF
+            
+            File reporte = new File(getServletConfig().getServletContext().getRealPath("/reportes/ReporteMonografias.jasper"));
+            
+            byte[] bytes = JasperRunManager.runReportToPdf(reporte.getPath(),null, con);//ARREGLO DE BYTES QUE VA A GUARDAR EL REPORTE
+            
+            response.setContentType("application/pdf");
+            response.setContentLength(bytes.length);
+            
+            sos.write(bytes, 0, bytes.length);//ES EL METODO QUE VA A DIBUJAR EL REPORTE
+            sos.flush();//LIMPIAMOS EL FLUJO DE SALIDA 
+            sos.close();//CERRAMOS LA SALIDA
+            
+        } catch (IOException | JRException ex) {
+            Logger.getLogger(Mono_Bio_Servlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+                       
     }
 
 }
